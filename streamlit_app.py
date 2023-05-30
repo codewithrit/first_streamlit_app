9427ff8175f3ed47b34361c81ce7e1c9b20b8209
Ritesh Tiwari <99679154+codewithrit@users.noreply.github.com>
2023-05-30 11:01:13 +0530

    Update streamlit_app.py
    add the breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-Breakfast Menu
-1.Bread Butter 
-2.Banana & Bread
+streamlit.header('Breakfast Menu')
+streamlit.text('Bread & banana')
+streamlit.text('Poha with banana')               
+streamlit.text('Bread Butter')               
